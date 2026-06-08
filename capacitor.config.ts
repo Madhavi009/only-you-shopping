5584ae285d53578bd9d8e3e5d6d895687e493113
Marri Madhavi <madhavimarri98@gmail.com>
2026-06-08 19:51:41 +0530

    Added webDir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.onlyshopping.app',
   appName: 'only you shopping',
+
+  webDir: 'out',
+
   server: {
     url: 'https://only-you-shopping.vercel.app',
     cleartext: true
