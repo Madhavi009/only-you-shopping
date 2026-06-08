2f45fe16d2ad7fd7b54d30333f135eae4e177646
Marri Madhavi <madhavimarri98@gmail.com>
2026-06-08 20:51:12 +0530

    Update capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,6 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.onlyshopping.app',
   appName: 'only you shopping',
-
-  webDir: '.next',
-
   server: {
     url: 'https://only-you-shopping.vercel.app',
     cleartext: true
